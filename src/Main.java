146bc90bbe8ca0e280ba6d6cc76af0807845eeb0
smth <danil.cherkasov@mail.ru>
2018-01-20 19:02:32 +0600

    i find so many bugs, fixing 1/2 of it...

--- src/Main.java
+++ src/Main.java
@@ -8,22 +8,35 @@ public class Main {
     public static void main(String args[]){
         MyCollection c = new MyVector();
         Vector<Integer> v = new Vector<>();
+
         c.add(1059);
-        v.add(1059);
+        c.add(258);
 
-        Random rnd = new Random(589);
+        c.set(0, 1);
+        c.set(1, 2);
 
-        for (int i = 1; i <= 10; ++i) {
-            c.add(rnd.nextInt(512));
-        }
-
-        rnd = new Random(589);
-        for (int i = 1; i <= 10; ++i) {
-            v.add(rnd.nextInt(512));
-        }
-
+//        v.add(1059);
+//
+//        Random rnd = new Random(589);
+//
+//        for (int i = 1; i <= 10; ++i) {
+//            c.add(rnd.nextInt(512));
+//        }
+//
+//        rnd = new Random(589);
+//        for (int i = 1; i <= 10; ++i) {
+//            v.add(rnd.nextInt(512));
+//        }
+//
         System.out.println(c.toString());
-        System.out.println(v.toString());
+//
+//        c.add(2,10);
+//        c.clear();
+//        c.remove(5);
+//        c.removeAll(1);
+//        c.removeAt(5);
+//        c.reverse();
+//        System.out.println(v.toString());
     }
 
 }
--- src/ThirdTask/MyVector.java
+++ src/ThirdTask/MyVector.java
@@ -15,50 +15,23 @@ public class MyVector extends MyCollection {
 
     @Override
     public void add(int element) {
-        int tmp[] = new int[array.length+1];
-        for(int i=0;i<array.length;i++){
-            tmp[i]=array[i];
-            array[i]=0;
-        }
-        tmp[tmp.length-1]=element;
-        array=tmp;
+
+
     }
 
     @Override
     public void add(int index, int element) {
-        int tmp[] = new int[0];
-        int i;
 
-        for(i=0;i<index;i++){
-            tmp[i]=array[i];
-            array[i]=0;
-        }
-        tmp[index]=element;
-        i++;
-        for(;i<array.length+1;i++){
-            tmp[i]=array[i-1];
-            array[i-1]=0;
-        }
-        for(i=0;i<array.length+1;i++){
-            array[i]=tmp[i];
-        }
     }
 
     @Override
     public void clear() {
-        for(int i=0;i<array.length+1;i++){
-            array[i]=0;
-        }
+
     }
 
     @Override
     public boolean contains(int o) {
-        for (int anArray : array) {
-            if (anArray == o) {
-                return true;
-            }
-        }
-        return false;
+
     }
 
     @Override
@@ -68,17 +41,12 @@ public class MyVector extends MyCollection {
 
     @Override
     public int indexOf(int o) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == o) {
-                return i;
-            }
-        }
-        return -1;
+
     }
 
     @Override
     public void insertElementAt(int element, int index) {
-        //
+
     }
 
     @Override
@@ -88,65 +56,22 @@ public class MyVector extends MyCollection {
 
     @Override
     public void removeAt(int index) {
-        int tmp[] = new int[0];
-        int i;
-        for(i=0;i<index;i++){
-            tmp[i]=array[i];
-            array[i]=0;
-        }
-        tmp[index]=0;
-        i++;
-        for(;i<array.length+1;i++){
-            tmp[i]=array[i-1];
-            array[i-1]=0;
-        }
-        for(i=0;i<array.length+1;i++){
-            array[i]=tmp[i];
-        }
+
     }
 
     @Override
     public void remove(int element) {
-        int tmp[] = new int[0];
-        int i;
-        boolean trigger=false;
-        for(i=0;i<array.length;i++){
-            if(array[i]==element&& !trigger) {
-                trigger=true;
-            }else {
-                tmp[i]=array[i];
-            }
-            array[i]=0;
-        }
-        for(i=0;i<array.length+1;i++){
-            array[i]=tmp[i];
-        }
+
     }
 
     @Override
     public void removeAll(int element) {
-        int tmp[] = new int[0];
-        int i;
-        int pos=0;
-        for(i=0;i<array.length;i++){
-            if(array[i]!=element) {
-                tmp[pos]=array[i];
-                pos++;
-            }
 
-        }
-        for(i=0;i<tmp.length;i++){
-            array[i]=tmp[i];
-        }
     }
 
     @Override
     public void reverse() {
-        for (int i = 0; i < array.length / 2 + 1; i++) {
-            int a = array[i];
-            array[i] = array[array.length - i];
-            array[array.length - i] = a;
-        }
+
     }
 
     @Override
@@ -161,15 +86,7 @@ public class MyVector extends MyCollection {
 
     @Override
     public void sort() {
-        for (int i = array.length - 1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-                if (array[j] > array[j + 1]) {
-                    int tmp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = tmp;
-                }
-            }
-        }
+
     }
 
     @Override
