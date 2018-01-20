55d83ccce968ae74912729ca45ac5cfedee8ec1d
smth-0 <danil.cherkasov@mail.ru>
2018-01-20 23:22:46 +0600

    re'maked class MyVector.

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,18 @@
 import ThirdTask.MyCollection;
 import ThirdTask.MyVector;
 
-import java.util.Random;
 import java.util.Vector;
 
 public class Main {
     public static void main(String args[]){
-        MyCollection c = new MyVector();
-        Vector<Integer> v = new Vector<>();
-
-        c.add(1059);
-        c.add(258);
-
-        c.set(0, 1);
-        c.set(1, 2);
-
-//        v.add(1059);
-//
-//        Random rnd = new Random(589);
-//
-//        for (int i = 1; i <= 10; ++i) {
-//            c.add(rnd.nextInt(512));
-//        }
-//
-//        rnd = new Random(589);
-//        for (int i = 1; i <= 10; ++i) {
-//            v.add(rnd.nextInt(512));
-//        }
-//
+        MyCollection c=new MyVector();
+        c.add(5);
+        c.add(1,6);
+        c.reverse();
+        c.sort();
         System.out.println(c.toString());
-//
-//        c.add(2,10);
-//        c.clear();
-//        c.remove(5);
-//        c.removeAll(1);
-//        c.removeAt(5);
-//        c.reverse();
-//        System.out.println(v.toString());
+
+
     }
 
 }
--- src/ThirdTask/MyVector.java
+++ src/ThirdTask/MyVector.java
@@ -1,10 +1,10 @@
 package ThirdTask;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class MyVector extends MyCollection {
 
-    private int[] array = new int[1];
+    private int[] array = new int[0];
 
     public MyVector() {}
 
@@ -15,23 +15,42 @@ public class MyVector extends MyCollection {
 
     @Override
     public void add(int element) {
-
-
+        int tmp[]=new int[array.length+1];
+        for(int i=0;i<array.length;i++){
+            tmp[i]=array[i];
+        }
+        tmp[array.length]=element;
+        array = new int[tmp.length];
+        array = tmp;
     }
 
     @Override
     public void add(int index, int element) {
-
+        int[] tmp = new int[array.length * 2];
+        index--;
+        for (int i = 0; i < index; i++) {
+            tmp[i] = array[i];
+        }
+        tmp[index] = element;
+        for (int i = index + 1; i < tmp.length; i++) {
+            tmp[i] = array[i - 1];
+        }
+        array = tmp;
     }
 
     @Override
     public void clear() {
-
+        array=new int[0];
     }
 
     @Override
     public boolean contains(int o) {
-
+        for(int i=0;i<array.length;i++){
+            if(array[i]==o){
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
@@ -41,12 +60,12 @@ public class MyVector extends MyCollection {
 
     @Override
     public int indexOf(int o) {
-
+return 1;
     }
 
     @Override
     public void insertElementAt(int element, int index) {
-
+        add(index,element);
     }
 
     @Override
@@ -56,22 +75,38 @@ public class MyVector extends MyCollection {
 
     @Override
     public void removeAt(int index) {
-
+        array[index-1]= Integer.parseInt(null);
     }
 
     @Override
     public void remove(int element) {
-
+        boolean trigger=false;
+        int i=0;
+        while (!trigger){
+            if(array[i]==element){
+                array[i]=0;
+                trigger=true;
+            }
+            i++;
+        }
     }
 
     @Override
     public void removeAll(int element) {
-
+        for(int i=0;i<array.length;i++){
+            if(array[i]==element){
+                array[i]=0;
+            }
+        }
     }
 
     @Override
     public void reverse() {
-
+        for (int i = 0; i < array.length / 2; i++) {
+            int a = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = a;
+        }
     }
 
     @Override
@@ -86,7 +121,15 @@ public class MyVector extends MyCollection {
 
     @Override
     public void sort() {
-
+        for(int i=0;i<array.length;i++){
+            for(int j=i;j<array.length;j++){
+                if(array[i]>array[j]){
+                    int c=array[i];
+                    array[i]=array[j];
+                    array[j]=c;
+                }
+            }
+        }
     }
 
     @Override
